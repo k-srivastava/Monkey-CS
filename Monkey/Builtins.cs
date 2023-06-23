68d7afe3a70a0f626705bec00a6252ef671c63df
Kshitij Srivastava <87322594+k-srivastava@users.noreply.github.com>
2023-06-23 17:21:35 +0530

    Edited error messages in builtin functions to be more helpful.

--- Monkey/Builtins.cs
+++ Monkey/Builtins.cs
@@ -24,24 +24,26 @@ public static class Builtins
     private static Object LenFunction(params Object[] arguments)
     {
         if (arguments.Length != 1)
-            return new Error($"Wrong number of arguments, expected 1 argument, got {arguments.Length}.");
+            return new Error($"Builtin function 'len' accepts only 1 argument, got {arguments.Length}.");
 
         return arguments[0] switch
         {
             String @string => new Integer(@string.Value.Length),
             Array array => new Integer(array.Elements.Length),
             Hash hash => new Integer(hash.Pairs.Count),
-            _ => new Error($"Builtin function 'len' is not supported for {arguments[0].Type}.")
+            _ => new Error(
+                $"Builtin function 'len' is only supported for Strings, Arrays and Hashes, not {arguments[0].Type}."
+            )
         };
     }
 
     private static Object FirstFunction(params Object[] arguments)
     {
         if (arguments.Length != 1)
-            return new Error($"Wrong number of arguments, expected 1 argument, got {arguments.Length}.");
+            return new Error($"Builtin function 'first' accepts only 1 argument, got {arguments.Length}.");
 
         if (arguments[0].Type != ObjectType.Array)
-            return new Error($"Builtin function 'first' is not supported for {arguments[0].Type}.");
+            return new Error($"Builtin function 'first' is only supported for Arrays, not {arguments[0].Type}.");
 
         var array = (arguments[0] as Array)!;
         return array.Elements.Length > 0 ? array.Elements[0] : Evaluator.Null;
@@ -50,10 +52,10 @@ public static class Builtins
     private static Object LastFunction(params Object[] arguments)
     {
         if (arguments.Length != 1)
-            return new Error($"Wrong number of arguments, expected 1 argument, got {arguments.Length}.");
+            return new Error($"Builtin function 'last' accepts only 1 argument, got {arguments.Length}.");
 
         if (arguments[0].Type != ObjectType.Array)
-            return new Error($"Builtin function 'last' is not supported for {arguments[0].Type}.");
+            return new Error($"Builtin function 'last' is only supported for Arrays, not {arguments[0].Type}.");
 
         var array = (arguments[0] as Array)!;
         return array.Elements.Length > 0 ? array.Elements[^1] : Evaluator.Null;
@@ -62,10 +64,10 @@ public static class Builtins
     private static Object RestFunction(params Object[] arguments)
     {
         if (arguments.Length != 1)
-            return new Error($"Wrong number of arguments, expected 1 argument, got {arguments.Length}.");
+            return new Error($"Builtin function 'rest' accepts only 1 argument, got {arguments.Length}.");
 
         if (arguments[0].Type != ObjectType.Array)
-            return new Error($"Builtin function 'last' is not supported for {arguments[0].Type}.");
+            return new Error($"Builtin function 'rest' is only supported for Arrays, not {arguments[0].Type}.");
 
         var array = (arguments[0] as Array)!;
         int length = array.Elements.Length;
@@ -96,7 +98,11 @@ public static class Builtins
             var newPairs = new Dictionary<HashKey, HashPair>();
 
             if (key is not IHashable hashableKey)
-                return new Error($"Key type for the hashmap is not a hashable type, got {key.Type}.");
+            {
+                return new Error(
+                    $"Builtin function 'push' for Hashes accepts only IHashable key types like Boolean, Integer and String, not {key.Type}."
+                );
+            }
 
             foreach ((HashKey hashKey, HashPair hashPair) in hash.Pairs) newPairs.Add(hashKey, hashPair);
             newPairs.Add(hashableKey.HashKey(), new HashPair(key, value));
@@ -104,41 +110,47 @@ public static class Builtins
             return new Hash(newPairs);
         }
 
-        if (arguments.Length is < 2 or > 3)
-            return new Error($"Wrong number of arguments, expected 2 or 3 arguments, got {arguments.Length}.");
+        if (arguments.Length is not (2 or 3))
+            return new Error($"Builtin function 'push' accepts only 2 or 3 arguments, got {arguments.Length}.");
 
         return arguments[0].Type switch
         {
             ObjectType.Array when arguments.Length == 2 => PushArrayFunction((Array)arguments[0], arguments[1]),
-            ObjectType.Array => new Error($"Wrong number of arguments, expected 2 arguments, got {arguments.Length}."),
+            ObjectType.Array => new Error(
+                $"Builtin function 'push' for Arrays accepts only 2 arguments, got {arguments.Length}."
+            ),
 
             ObjectType.Hash when arguments.Length == 3 => PushHashFunction(
                 (Hash)arguments[0], arguments[1], arguments[2]
             ),
-            ObjectType.Hash => new Error($"Wrong number of arguments, expected 3 arguments, got {arguments.Length}."),
+            ObjectType.Hash => new Error(
+                $"Builtin function 'push' for Arrays accepts only 3 arguments, got {arguments.Length}."
+            ),
 
-            _ => new Error($"Builtin function 'push' is not supported for {arguments[0].Type}.")
+            _ => new Error($"Builtin function 'rest' is only supported for Arrays and Hashes, not {arguments[0].Type}.")
         };
     }
 
     private static Object MapFunction(params Object[] arguments)
     {
         if (arguments.Length != 2)
-            return new Error($"Wrong number of arguments, expected 2 arguments, got {arguments.Length}.");
+            return new Error($"Builtin function 'map' accepts only 2 arguments, got {arguments.Length}.");
 
         if (arguments[0].Type != ObjectType.Array)
-            return new Error($"Builtin function 'map' is not supported on {arguments[0].Type}.");
+            return new Error($"Builtin function 'map' can only map onto Arrays, not {arguments[0].Type}.");
 
         if (arguments[1].Type != ObjectType.Function)
-            return new Error($"Builtin function 'map' is not supported for {arguments[1].Type}.");
+            return new Error($"Builtin function 'map' can only map from Functions, not {arguments[1].Type}.");
 
         var array = (Array)arguments[0];
         var function = (Function)arguments[1];
 
         if (function.Parameters.Length != 1)
+        {
             return new Error(
                 $"Function passed to builtin function 'map' should have only 1 parameter, not {function.Parameters.Length}."
             );
+        }
 
         var newElements = new Object[array.Elements.Length];
         for (var i = 0; i < array.Elements.Length; i++)
@@ -153,34 +165,38 @@ public static class Builtins
     private static Object ReduceFunction(params Object[] arguments)
     {
         if (arguments.Length != 3)
-            return new Error($"Wrong number of arguments, expected 3 arguments, got {arguments.Length}.");
+            return new Error($"Builtin function 'reduce' accepts only 3 arguments, got {arguments.Length}.");
 
         if (arguments[0].Type != ObjectType.Array)
-            return new Error($"Builtin function 'reduce' is not supported on {arguments[0].Type}.");
+            return new Error($"Builtin function 'reduce' can only reduce Arrays, not {arguments[0].Type}.");
 
         if (arguments[1].Type != ObjectType.Integer)
-            return new Error($"Builtin function 'reduce' is not supported for {arguments[1].Type}.");
+            return new Error($"Builtin function 'reduce' can only reduce into Integers, not {arguments[1].Type}.");
 
         if (arguments[2].Type != ObjectType.Function)
-            return new Error($"Builtin function 'reduce' is not supported for {arguments[2].Type}.");
+            return new Error($"Builtin function 'reduce' can only reduce from Functions, not {arguments[2].Type}.");
 
         var array = (Array)arguments[0];
         var start = (Integer)arguments[1];
         var function = (Function)arguments[2];
 
         if (function.Parameters.Length != 2)
+        {
             return new Error(
                 $"Function passed into builtin function 'reduce' should have only 2 parameters, not {function.Parameters.Length}."
             );
+        }
 
         Integer output = start;
         for (var i = 0; i < array.Elements.Length; i++)
         {
             Object @object = array.Elements[i];
             if (@object.Type != ObjectType.Integer)
+            {
                 return new Error(
-                    $"Element at index [{i}] in array passed into builtin function 'reduce' should be an Integer, not {@object.Type}."
+                    $"Element at index [{i}] in Array passed into builtin function 'reduce' should be an Integer, not {@object.Type}."
                 );
+            }
 
             output = (Integer)Evaluator.ApplyFunction(function, new[] { output, @object });
         }
@@ -191,10 +207,10 @@ public static class Builtins
     private static Object SumFunction(params Object[] arguments)
     {
         if (arguments.Length != 1)
-            return new Error($"Wrong number of arguments, expected 1 argument, got {arguments.Length}.");
+            return new Error($"Builtin function 'sum' accepts only 1 arguments, got {arguments.Length}.");
 
         if (arguments[0].Type != ObjectType.Array)
-            return new Error($"Builtin function 'sum' is not supported for {arguments[0].Type}.");
+            return new Error($"Builtin function 'sum' can only sum Arrays, not {arguments[0].Type}.");
 
         var array = (Array)arguments[0];
         long sum = 0;
@@ -204,7 +220,7 @@ public static class Builtins
             Object @object = array.Elements[i];
             if (@object.Type != ObjectType.Integer)
                 return new Error(
-                    $"Element at index [{i}] in array passed into builtin function 'sum' should be an Integer, not {@object.Type}."
+                    $"Element at index [{i}] in Array passed into builtin function 'sum' should be an Integer, not {@object.Type}."
                 );
 
             sum += ((Integer)@object).Value;
